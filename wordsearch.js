aa8a3ed1898cf2b67aac7e00e3369c60fd81904a
Louis Manlangit <louism.coding@gmail.com>
2021-07-28 21:45:26 +0000

    Added horizontal and vertical search functions of a word in a matrix

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let output = [];
+    for (let row in matrix[0]) {
+      let array = [];
+      for (let column in matrix) {
+        array.push(matrix[column][row]);
+      }
+      output.push(array);
     }
-}
-
-module.exports = wordSearch
+    return output;
+  };
+  
+  const wordSearch = (letters, word) => {
+    let wordFound = false;
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) {
+        wordFound = true;
+      }
+    }
+    const transposeSearch = transpose(letters);
+    const verticalJoin = transposeSearch.map(lts => lts.join(''));
+    for (let lts of verticalJoin) {
+      if (lts.includes(word)) {
+        wordFound = true;
+      }
+    }
+    return wordFound;
+  };
+  
+  module.exports = wordSearch;
