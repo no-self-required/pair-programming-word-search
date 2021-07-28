14b550f1ed1819c480b04263e44a92cd458bb37a
Louis Manlangit <louism.coding@gmail.com>
2021-07-28 21:49:41 +0000

    fixed linting

--- wordsearch.js
+++ wordsearch.js
@@ -1,31 +1,31 @@
 const transpose = function(matrix) {
-    let output = [];
-    for (let row in matrix[0]) {
-      let array = [];
-      for (let column in matrix) {
-        array.push(matrix[column][row]);
-      }
-      output.push(array);
+  let output = [];
+  for (let row in matrix[0]) {
+    let array = [];
+    for (let column in matrix) {
+      array.push(matrix[column][row]);
     }
-    return output;
-  };
+    output.push(array);
+  }
+  return output;
+};
   
-  const wordSearch = (letters, word) => {
-    let wordFound = false;
-    const horizontalJoin = letters.map(ls => ls.join(''));
-    for (let l of horizontalJoin) {
-      if (l.includes(word)) {
-        wordFound = true;
-      }
+const wordSearch = (letters, word) => {
+  let wordFound = false;
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      wordFound = true;
     }
-    const transposeSearch = transpose(letters);
-    const verticalJoin = transposeSearch.map(lts => lts.join(''));
-    for (let lts of verticalJoin) {
-      if (lts.includes(word)) {
-        wordFound = true;
-      }
+  }
+  const transposeSearch = transpose(letters);
+  const verticalJoin = transposeSearch.map(lts => lts.join(''));
+  for (let lts of verticalJoin) {
+    if (lts.includes(word)) {
+      wordFound = true;
     }
-    return wordFound;
-  };
+  }
+  return wordFound;
+};
   
-  module.exports = wordSearch;
+module.exports = wordSearch;
